4de7b45e56e55c11b9883229dc20550134c169c0
Ervin <hamoydoerik@gmail.com>
2021-10-11 13:25:50 +0300

    Added functionality for creating a post.

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,15 +1,49 @@
-import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React, { useState } from "react";
+import {View, Text, StyleSheet, TextInput, Image, Button, ScrollView, TouchableWithoutFeedback, Keyboard} from "react-native";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
+import {useDispatch} from 'react-redux';
 
 import { AppHeaderIcon } from "../components/AppHeaderIcon";
+import { addPost } from "../store/actions/post";
+import { THEME } from "../theme";
 
-export const CreateScreen = ({}) => {
+export const CreateScreen = ({navigation}) => {
+   const [text, setText] = useState('');
+   const img = 'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg';
+
+   const dispatch = useDispatch();
+
+   const saveHandler = () => {
+      const post = {
+         date: new Date().toJSON(),
+         text: text,
+         booked: false,
+         img
+      };
+
+      dispatch(addPost(post));
+      navigation.navigate('Main');
+   }
 
    return (
-      <View style={styles.center}>
-         <Text>Create screen</Text>
-      </View>
+      <ScrollView style={styles.wrapper}>
+         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View>
+               <Text style={styles.title}>Create a new post</Text>
+
+               <TextInput
+                  style={styles.textarea}
+                  placeholder='Enter the post text'
+                  value={text}
+                  onChangeText={setText}
+                  multiline
+               />
+
+               <Image style={{width: '100%', height: 200, marginBottom: 10}} source={{uri: img}} />
+               <Button title='Create the post' color={THEME.MAIN_COLOR} onPress={saveHandler} />
+            </View>
+         </TouchableWithoutFeedback>
+      </ScrollView>
    )
 }
 
@@ -27,9 +61,17 @@ CreateScreen.navigationOptions = ({navigation}) => ({
 })
 
 const styles = StyleSheet.create({
-   center: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
+   wrapper: {
+      padding: 10
+   },
+   title: {
+      fontSize: 20,
+      textAlign: 'center',
+      fontFamily: 'open-sans-regular',
+      marginVertical: 10
+   },
+   textarea: {
+      padding: 10,
+      marginBottom: 10
    }
 })
